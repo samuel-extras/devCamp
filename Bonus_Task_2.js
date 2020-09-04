f20e27ebdb85cc51ad64beefa77b714b05f17de7
samuel-extras <samuel88783.so@gmail.com>
2020-09-04 16:35:20 +0100

    adjusting tsk

--- Bonus_Task_2.js
+++ Bonus_Task_2.js
@@ -1,23 +1,20 @@
-const consonantReplacer =(string)=>{
-    let replace = ""
-    let consonant = "bcdfghjklmnpqrstvwxyz";
-    let vowel = ["a","e","i","o","u"]
-    let consonants = consonant.split()
-    for (let i = 0; i < string.length; i++) {
-        if (vowel.includes(string[i])){
-           return string[i]
-            
-        }
-        
-        for (let j = 0; j < consonants.length; j++) {
-            if(consonants[j].includes(string[i])){
-              return  string[i] === consonants[j+1]
-
+const consonantReplacer = (string) => {
+  let replace = "";
+  const consonant = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+  const vowel = ["a", "e", "i", "o", "u"];
+  
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] ==="a" || string[i] ==="e" || string[i] ==="i" || string[i] ==="o" || string[i] ==="u" ) {
+        console.log(string[i]);
+    } else if (consonant.includes(string[i])){
+        for (let j = 0; j < consonant.length; j++) {
+            if(string.includes(consonant[j])){
+                console.log(consonant[j +1]);
             }
             
         }
-        return
     }
-
-}
-consonantReplacer("ajibola")
+    
+  }
+};
+consonantReplacer("ajibola");
