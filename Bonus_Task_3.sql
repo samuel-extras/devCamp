2940f879f71680441ef2a18e36273c00fa749369
Samuel Ajibola <56566766+samuel-extras@users.noreply.github.com>
2020-09-04 10:55:49 +0100

    Delete Bonus_Task_3.sql

--- Bonus_Task_3.sql
+++
@@ -1,4 +0,0 @@
-SELECT TenantID, AptID COUNT(*) TenantID FROM AptTenants GROUP BY TenantID, AptID HAVING COUNT(*) > 1
-/* Question ii */
-SELECT * from Buildings
-SELECT * from   Requests WHERE Status = 'open'
