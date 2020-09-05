b8b104f291c7ed22861b1e411a5b1f1dae7cfa3d
samuel-extras <samuel88783.so@gmail.com>
2020-09-05 16:33:46 +0100

    adding more func

--- Bonus_Task_1.sql
+++ Bonus_Task_1.sql
@@ -1,5 +1,5 @@
-/* bonus i*/
+/* bonus 2*/
 SELECT TenantID, AptID COUNT(*) TenantID FROM AptTenants GROUP BY TenantID, AptID HAVING COUNT(*) > 1
-/* bonus ii */
+/* bonus ii*/
 SELECT * from Buildings
 SELECT * from   Requests WHERE Status = "Open"
--- Task_6.js
+++ Task_6.js
@@ -1,10 +1,7 @@
 const addUpToNumber = (array, number) => {
-    let addUpArray = [];
-    let newArray = array.sort()
-    let sum = 0
+  let addUpArray = [];
+  let newArray = array.sort();
+  let sum = 0;
 
-   
-
-    
-}
-addUpToNumber([7,1,6,2,3,9,5,6,7,8], 6)
+  addUpToNumber([7, 1, 6, 2, 3, 9, 5, 6, 7, 8], 6);
+};
